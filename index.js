4e1c7553245e10f73688cd9358f595b4ccb1897d
Brian <brianmakorious@gmail.com>
2022-12-07 05:23:58 +0300

    complete assignment

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function addingEventListener() {
  const clickMe = document.getElementById('button') ;
  button.addEventListener('click', () => {alert("you clicked me!")})
  }
- 
  addingEventListener()
